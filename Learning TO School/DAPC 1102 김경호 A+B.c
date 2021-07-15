b395db385350810a8ccce352fe6c0d2a2026b61b
김경호 <nestjs01@naver.com>
2021-07-15 18:39:32 +0900

    DAPC 예시문제1

---
+++ Learning TO School/DAPC 1102 김경호 A+B.c
@@ -0,0 +1,9 @@
+#pragma warning(disable:4996)
+#pragma warning(disable:6013)
+#include<stdio.h>
+int main() {
+	int num1 = 0, num2 = 0;
+	scanf("%d %d", &num1, &num2);
+	printf("%d\n", num1 + num2);
+	return 0;
+}
