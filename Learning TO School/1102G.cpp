c959d1ea28a56b221d6aa33964225c3c47616a3c
김경호 <nestjs01@naver.com>
2021-07-19 00:36:52 +0900

    .

--- Learning TO School/1102G.cpp
+++ Learning TO School/1102G.cpp
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main(){
 	int n;
+	int i = 0;
 	int str = 0;
 	int arr[1000] = {0,};
 	int arr1[1000] = {0,};
 	scanf("%d", &n);
-	for(int i = 0;i<n;i++){
+	for(i = 0;i<n;i++){
 		scanf("%d", &arr[i]);
 	}
-	for(int i = 0;i<n;i++){
+	for(i = 0;i<n;i++){
 		scanf("%d", &arr1[i]);
 	}
-	for(int i = 0;i<n;i++){
-		
-		while(1){
-			int j = n-i-1;
+	for(i = 0;i<n;i++){
+		int j = n-i-1;
+		for(int k = 0;k<j;k++){
 			if(arr[i]!=arr1[i]){
 					if(arr[i]>arr1[i]){
 						arr1[i]++;
@@ -27,13 +27,19 @@ int main(){
 						str++;
 					}
 				}
-				else break;
+			else break;	
 		}
 	}
-	printf("%d", str);
-	
+	/*for(i = 0;i<n;i++){
+		
+	}*/
+	printf("%d\n", str);
+	return 0;
 }
 //7
 //11 63 59 35 46 77 67
 //93 26 85 74 30 24 26
+//4
+//1 3 5 7
+//7 5 2 1
 
