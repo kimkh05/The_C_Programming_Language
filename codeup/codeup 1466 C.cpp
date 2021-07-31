7e4dbeaf05e23aa99fb5cf648d249a1dbff3f1e5
김경호 <nestjs01@naver.com>
2021-07-31 23:15:08 +0900

    ckodeup 1466

---
+++ codeup/codeup 1466 C.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int n, m;
+	int sum = 0;
+	int arr[100][100] = {0, };
+	scanf("%d %d", &n, &m);
+	for(int j = m-1;j>=0;j--){
+		for(int i = n-1;i>=0;i--){
+			sum++;
+			arr[i][j] = sum;
+		}
+	}
+	for(int i = 0;i<n;i++){
+		for(int j = 0;j<m;j++){
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
