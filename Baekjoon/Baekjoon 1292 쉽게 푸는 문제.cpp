9e9061fa7a7ea5b96d51f6cf256dc1e715225a24
김경호 <nestjs01@naver.com>
2021-08-19 17:13:44 +0900

    Baekjoon 1292

---
+++ Baekjoon/Baekjoon 1292 쉽게 푸는 문제.cpp
@@ -0,0 +1,21 @@
+/*#include<stdio.h>
+int main(){
+	int arr[1001] = {0, };
+	int a = 0, q, p, i, j, k;
+	int sum = 0;
+	int cnt = 1;
+	scanf("%d %d", &p, &q);
+	for(i = 1 ; i<=1000;i++){
+		for(j = 1;j<=i;j++){
+			arr[a] = i;
+			if(a>1000) break;
+			a++;
+		}
+	}
+	for(i = p-1;i<q;i++){
+		sum+=arr[i];
+	}
+	printf("%d\n", sum);
+	return 0;
+}*/
+
