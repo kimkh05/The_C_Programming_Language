2130113548a80b8f7ca3a8a150cfb8d95f25332b
김경호 <nestjs01@naver.com>
2021-08-20 15:52:43 +0900

    C++ cout

---
+++ Learning TO School/C++ cout.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){  //main 함수, 가장 처음 실행 되는 함수{
+	std::cout << "Hello World!\n"; // std::cout을 이용하여 "Hello World"를 출력
+	std::cout << "이 프로그램은 프로그램의 구조를 알아보기 위한 ";
+	std::cout << "간단한 C++ 프로그램입니다." << std::endl;
+	std::cout << "이번 장과 이후의 내용을 통해 ";
+	std::cout << "C++프로그래밍 언어와 관련된 다양한 것을 살펴보겠습니다. "<< std::endl;
+	std::cout << 1 << "1 " << 1.29380 << 'a' << std::endl;
+}
