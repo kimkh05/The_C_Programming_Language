6e6e8bd70d2ee18fc289df077a979489fa936cfe
김경호 <nestjs01@naver.com>
2021-08-20 15:53:07 +0900

    C++ std::cout으로 사각형모양 출력

---
+++ Learning TO School/C++ 별 기호로 사각형 출력.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int main(){
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 5; j++) {
+			std::cout << "*";
+		}
+		std::cout << " " << std::endl;
+	}
+	// 별 기호로 사각형 출력
+//	std::cout << "******" << std::endl;
+//	std::cout << "******" << std::endl;
+//	std::cout << "******" << std::endl;
+//	std::cout << "******" << std::endl;
+//	std::cout << "******" << std::endl;
+//	std::cout << "******" << std::endl;
+//	return 0;
+}
