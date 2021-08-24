5579ea33df36aefb2cff96b5f026985c735ec802
김경호 <nestjs01@naver.com>
2021-08-24 19:30:01 +0900

    C++

---
+++ Learning TO School/C++/C++ Dynamic_Allocation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main()
+{
+    // 인스터스만 동적으로 생성하는 경우
+    int* pData = new int;
+
+    // 초기값을 기술하는 경우
+    int* pNewData = new int(10);
+
+    *pData = 5;
+    std::cout << *pData << std::endl;
+    std::cout << *pNewData << std::endl;
+
+    delete pData;
+    delete pNewData;
+    
+    return 0;
+}
---
+++ Learning TO School/C++/C++ Dynamic_Allocation2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main()
+{
+    // 객체를 배열 형태로 동적 생성한다.
+    int* arr = new int[5];
+
+    for (int i = 0; i < 5; i++)
+        arr[i] = (i + 1) * 10;
+
+    for (int i = 0; i < 5; i++)
+        std::cout << arr[i] << std::endl;
+
+    // 배열 형태로 생성한 대상은 
+    // 반드시 배열 형태를 통해 삭제한다!
+    delete[] arr;
+
+    return 0;
+}
