9cfb06b34bb8049e59094757f3f67c6238cc14c7
김경호 <nestjs01@naver.com>
2021-09-07 17:17:19 +0900

    Baekjoon 2751

--- Baekjoon/Baekjoon 2751 수 정렬하기 2.cpp
+++ Baekjoon/Baekjoon 2751 수 정렬하기 2.cpp
@@ -1,24 +1,21 @@
 #include<stdio.h>
-#include<iostream>
-using namespace std;
-void bubble_sorting(int arr[], int n){
-	int temp;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n - 1; j++) {
-            if (arr[j] > arr[j + 1]){
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;           
-			}
-        }
-    }
-}
 int main(){
-	int num[1000001];
 	int n;
+	int temp;
+	int arr[1000000];
 	scanf("%d", &n);
-	for(int i = 0;i<n;i++) scanf("%d", &num[i]);
-	bubble_sorting(num, n);
-	for(int i = 0;i<n;i++) cout << num[i];
+	for(int i = 0;i<n;i++){
+		scanf("%d", &arr[i]);
+	}
+	for(int i = 0;i<n-1;i++){
+		for(int j = 1;j<n;j++){
+			if(arr[i]>arr[j]){
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	for(int i = 0;i<n;i++) printf("%d\n", arr[i]);
 	return 0;
 }
